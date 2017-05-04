9ed165ad489904d18721d46a4966b0126752b2d9
admin <admin@A2096GOS11>
2017-05-04 09:37:09 +0200

    Dut us een aap,assiong

--- githubprojecttest/Program.cs
+++ githubprojecttest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace githubprojecttest
 {
@@ -10,6 +11,9 @@ namespace githubprojecttest
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hallo Fabian");
+            Console.ReadLine();
             
         }
     }
